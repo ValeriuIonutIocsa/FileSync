26e27a3c9534db7015d9e6508e695711737e759a
Valeriu Ionut Iocsa <valeriuionutiocsa@gmail.com>
2023-05-04 17:24:39 +0300

    updated dependencies

--- Projects/Personal/FileSync/FileSync/src/test/java/com/personal/scripts/gen/fs/AppStartFileSyncTest.java
+++ Projects/Personal/FileSync/FileSync/src/test/java/com/personal/scripts/gen/fs/AppStartFileSyncTest.java
@@ -8,9 +8,11 @@ class AppStartFileSyncTest {
 	void testMain() {
 
 		final String[] args;
-		final int input = Integer.parseInt("1");
+		final int input = Integer.parseInt("2");
 		if (input == 1) {
 			args = new String[] { "D:\\tmp\\FileSync\\FileSyncSettings.xml" };
+		} else if (input == 2) {
+			args = new String[] { "C:\\IVI\\Apps\\FileSync\\FileSyncSettings_PcToExt_IVI.xml" };
 
 		} else if (input == 101) {
 			args = new String[] { "" };
